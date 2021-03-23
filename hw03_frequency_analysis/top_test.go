86b9f7cbfbe799bb75cc377f948d774f8a14f8d5
Max Zinchenko <mxszin@gmail.com>
2021-03-23 23:15:53 +0300

    HW3 is completed

--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -6,9 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Change to true if needed.
-var taskWithAsteriskIsCompleted = false
-
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
 	ступеньки собственным затылком:  бум-бум-бум.  Другого  способа
@@ -44,13 +41,15 @@ var text = `Как видите, он  спускается  по  лестни�
 		В этот вечер...`
 
 func TestTop10(t *testing.T) {
-	t.Run("no words in empty string", func(t *testing.T) {
-		require.Len(t, Top10(""), 0)
-	})
-
-	t.Run("positive test", func(t *testing.T) {
-		if taskWithAsteriskIsCompleted {
-			expected := []string{
+	tests := []struct {
+		name     string
+		input    string
+		expected []string
+	}{
+		{
+			name:  "main case",
+			input: text,
+			expected: []string{
 				"а",         // 8
 				"он",        // 8
 				"и",         // 6
@@ -61,22 +60,41 @@ func TestTop10(t *testing.T) {
 				"если",      // 4
 				"кристофер", // 4
 				"не",        // 4
-			}
-			require.Equal(t, expected, Top10(text))
-		} else {
-			expected := []string{
-				"он",        // 8
-				"а",         // 6
-				"и",         // 6
-				"ты",        // 5
-				"что",       // 5
-				"-",         // 4
-				"Кристофер", // 4
-				"если",      // 4
-				"не",        // 4
-				"то",        // 4
-			}
-			require.Equal(t, expected, Top10(text))
-		}
-	})
+			},
+		},
+		{
+			name:     "no words in empty string",
+			input:    "",
+			expected: []string{},
+		},
+		{
+			name:  "less then 10 words",
+			input: "one two two",
+			expected: []string{
+				"two", "one",
+			},
+		},
+		{
+			name:  "punctuation marks",
+			input: "one, two! two; three. three? :three!",
+			expected: []string{
+				"three", "two", "one",
+			},
+		},
+		{
+			name:  "sentence with dashes",
+			input: "- a b c- d-d -",
+			expected: []string{
+				"a", "b", "c-", "d-d",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			result := Top10(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
 }
